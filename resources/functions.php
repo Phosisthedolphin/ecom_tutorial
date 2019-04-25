9b71b47823355e60d71a4e121372be372b277aec
Joel Peterson <joelp@gbmtrailer.ca>
2019-04-25 14:20:12 -0600

    end of day commit

--- resources/functions.php
+++ resources/functions.php
@@ -4,4 +4,32 @@ if($connection) {
     echo "is connected";
 }
 
+function redirect($location) {
+    header("Location: $location ");
+}
+
+global $connection;
+
+function query($sql) {
+    return mysqli_query($connection, $sql);
+}
+
+function confirm($result) {
+    global $connection;
+
+    if(!$result) {
+        die("QUERY FAILED " . mysqli_error($connection));
+    }
+}
+
+function escape_string($string) {
+    global $connection;
+
+    return mysql_real_escape_string($connection, $string);
+}
+
+function fetch_array($result) {
+    return mysqli_fetch_array($result)
+}
+
 ?>
