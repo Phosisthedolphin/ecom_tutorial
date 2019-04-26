d5a8b26cf7390bddbdf6f4df885737ba052f1290
Joel Peterson <joelp@gbmtrailer.ca>
2019-04-26 10:27:26 -0600

    first commit of day

--- resources/functions.php
+++ resources/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+// helper functions
+
 if($connection) {
     echo "is connected";
 }
@@ -11,6 +13,9 @@ function redirect($location) {
 global $connection;
 
 function query($sql) {
+
+    global $connection;
+
     return mysqli_query($connection, $sql);
 }
 
@@ -29,7 +34,42 @@ function escape_string($string) {
 }
 
 function fetch_array($result) {
-    return mysqli_fetch_array($result)
+    return mysqli_fetch_array($result);
+}
+
+// get products
+
+function get_products() {
+    // Using the query helper function to return mysqli_query from SQL database
+    $query = query(" SELECT * FROM products");
+
+    // Use the global confirm function to check and see if a result is being loaded. If not, die.
+
+    confirm($query);
+
+    while($row = fetch_array($query)) {
+
+    $product = <<<DELIMETER
+    
+    <div class="col-sm-4 col-lg-4 col-md-4">
+        <div class="thumbnail">
+            <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+                <h4 class="pull-right">{$row['product_price']}</h4>
+                <h4><a href="product.html">{$row['product_title']}</a>
+                </h4>
+                <p>{$row['product_description']}</p>
+                <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">Buy Now</a>
+            </div>
+            
+        </div>
+    </div>
+
+DELIMETER;
+
+echo $product;
+
+    }
 }
 
 ?>
